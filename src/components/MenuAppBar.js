7e5a5078096696d7e15fe27660f37ed308cd8f15
HRSArgyropoulos <13-13196@saeinstitute.edu>
2021-09-19 15:39:43 +0300

    theme / drawer / appbar

---
+++ src/components/MenuAppBar.js
@@ -0,0 +1,77 @@
+import { React, useState } from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import IconButton from '@mui/material/IconButton';
+import AccountCircle from '@mui/icons-material/AccountCircle';
+import MenuItem from '@mui/material/MenuItem';
+import Menu from '@mui/material/Menu';
+import FastfoodIcon from '@mui/icons-material/Fastfood';
+import MenuDrawer from './MenuDrawer';
+
+const MenuAppBar = () => {
+  const auth = true;
+
+  const [anchorEl, setAnchorEl] = useState(null);
+
+  const handleMenu = (event) => {
+    console.log(event);
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar position="static">
+        <Toolbar>
+          <MenuDrawer />
+          <Typography
+            variant="h6"
+            color="secondary"
+            component="div"
+            sx={{ flexGrow: 1 }}>
+            <FastfoodIcon sx={{ mr: 1 }} />
+            Foodiee
+          </Typography>
+          {auth && (
+            <div>
+              <IconButton
+                size="large"
+                aria-label="account of current user"
+                aria-controls="menu-appbar"
+                aria-haspopup="true"
+                onClick={handleMenu}
+                color="inherit">
+                <AccountCircle />
+              </IconButton>
+              <Menu
+                id="menu-appbar"
+                anchorEl={anchorEl}
+                anchorOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                keepMounted
+                transformOrigin={{
+                  vertical: 'top',
+                  horizontal: 'right',
+                }}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}>
+                <MenuItem onClick={handleClose}>Profile</MenuItem>
+                <MenuItem onClick={handleClose}>Adresses</MenuItem>
+                <MenuItem onClick={handleClose}>Orders</MenuItem>
+              </Menu>
+            </div>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+};
+
+export default MenuAppBar;
