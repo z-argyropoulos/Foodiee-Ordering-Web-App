b0b286737b5e2ba6c13a23f4e78a1143fc576328
Haris Argyropoulos <harinioarg@gmail.com>
2021-11-14 18:35:23 +0200

    Remove apollo client and name changing

--- client/src/index.js
+++ client/src/index.js
@@ -3,25 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import {
-  ApolloClient,
-  InMemoryCache,
-  ApolloProvider,
-} from '@apollo/client';
 import { BrowserRouter } from 'react-router-dom';
-
-const client = new ApolloClient({
-  uri: process.env.REACT_APP_SERVER_GRAPHQL_API,
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </ApolloProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
