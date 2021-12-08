899c66703200b7e99ec7f4c79260a0ff2758121d
Haris Argyropoulos <harinioarg@gmail.com>
2021-12-08 16:40:01 +0200

    store catalog styling/presentation

--- client/src/components/StoreProductCatalog.js
+++ client/src/components/StoreProductCatalog.js
@@ -2,28 +2,65 @@ import { Card, Typography, Grid } from '@mui/material';
 
 const StoreProductCatalog = ({ catalog }) => {
   return (
-    <Card>
-      <Typography variant="h4">Catalog</Typography>
+    <>
+      <Typography variant="h4" sx={{ textAlign: 'center', my: 1 }}>
+        Catalog
+      </Typography>
       <Grid container>
         {catalog.map(({ category, products }) => (
-          <Grid item key={category}>
-            <Typography variant="h5">{category}</Typography>
-            <Grid container>
-              {products.map(
-                ({ _id, title, description, price, quantity }) => (
-                  <Grid item key={_id}>
-                    {title}
-                    {description}
-                    {price}
-                    {quantity}
-                  </Grid>
-                )
-              )}
+          <Grid item key={category} sx={{ my: 2 }}>
+            <Typography variant="h5" sx={{ mb: 4 }}>
+              {category}
+            </Typography>
+            <Grid item container rowSpacing={1}>
+              {products.map(({ _id, title, description, price }) => (
+                <Grid item key={_id} sm={12}>
+                  <Card
+                    sx={{
+                      maxWidth: '1000px',
+                      height: { sm: '140px' },
+                      borderRadius: 2,
+                      cursor: 'pointer',
+                    }}>
+                    <Grid container>
+                      <Grid item xs={12} sm={4}>
+                        <img
+                          style={{
+                            width: '100%',
+                            objectFit: 'cover',
+                            objectPosition: 'center',
+                          }}
+                          src={`${process.env.PUBLIC_URL}/img/restaurants/products/${_id}.jpg`}
+                          alt={title}
+                        />
+                      </Grid>
+                      <Grid item xs={12} sm={8}>
+                        <Grid
+                          container
+                          direction="column"
+                          sx={{ px: 3, my: 1, py: 2 }}>
+                          <Typography variant="h6">
+                            {title}
+                          </Typography>
+                          <div>{description}</div>
+                          <div
+                            style={{
+                              marginTop: '1em',
+                              color: 'yellow',
+                            }}>
+                            {price} €
+                          </div>
+                        </Grid>
+                      </Grid>
+                    </Grid>
+                  </Card>
+                </Grid>
+              ))}
             </Grid>
           </Grid>
         ))}
       </Grid>
-    </Card>
+    </>
   );
 };
 
